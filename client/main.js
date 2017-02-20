4526a82358ef64c789a9ee4aba78ad3641adf04f
Gerwin Brunner <gerwin.brunner@gmail.com>
2017-02-20 20:23:41 +0100

    added test for method

--- client/main.js
+++ client/main.js
@@ -10,14 +10,6 @@ const learnedWords = new Mongo.Collection('testColl');
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
-  const handle = Meteor.subscribe('test_pub', 'XX');
-  this.autorun(function(){
-    if ( handle.ready() ) {
-    const lw = learnedWords.findOne();
-    console.log( 'lw:', lw);
-    console.log( 'lw:', lw.learnedWords.length );      
-    }
-  })
 });
 
 Template.hello.helpers({
@@ -27,8 +19,19 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+  'click button.methodTest'(event, instance) {
+    Meteor.call('testMethod', 'YY', function (error, result) {
+        console.log('test_method',error, result);
+    });
   },
+  'click button.subTest'(event, instance) {
+    const handle = Meteor.subscribe('test_pub', 'XX');
+    Template.instance().autorun(function(){
+      if ( handle.ready() ) {
+        const lw = learnedWords.findOne();
+        console.log( 'lw:', lw);
+        console.log( 'lw:', lw.learnedWords.length );
+      }
+    });
+  },  
 });
--- server/main.js
+++ server/main.js
@@ -1,19 +1,31 @@
 import { Meteor } from 'meteor/meteor';
 import { _ } from 'lodash';
+import { Random } from 'meteor/random';
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
 
+const ran = Random.id();
+const lWs = [];
+_.times(10001001, (idx) => {
+  // if (idx % 100000 === 0) {
+  //   console.log('-', idx);
+  // }
+  lWs.push(ran+idx);
+});
+
+
+Meteor.methods({
+  testMethod: function(code) {
+    console.log("testMethod - " + code);
+    return {code: { learnedWords: lWs }};
+  }
+});
+
 Meteor.publish('test_pub', function(code) {
   console.log('test_pub', code);
   const self = this;
-
-  const lWs = [];
-  _.times(10000000, (idx) => {
-    lWs.push(idx);
-  });
-
   self.added("testColl", code, { learnedWords: lWs });
   self.ready();
 });
